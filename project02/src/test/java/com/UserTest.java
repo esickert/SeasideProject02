438c1e3edf23e40087ac536261a4463803a26f3e
Erich Sickert <esickert@gmail.com>
2017-01-01 15:18:35 -0800

    plus

---
+++ project02/src/test/java/com/UserTest.java
@@ -0,0 +1,15 @@
+package com;
+
+import com.esickert.domainentities.User;
+import org.junit.Test;
+
+/**
+ * Created by esickert on 1/1/2017.
+ */
+public class UserTest {
+    @Test
+    public void canConstructNewUser()   {
+        User user = new User();
+    }
+
+}
