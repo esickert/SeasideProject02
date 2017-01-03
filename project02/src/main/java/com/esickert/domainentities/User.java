4a51bdf923b609dc643a50e171dbb6865702cb72
Erich Sickert <esickert@gmail.com>
2017-01-02 17:14:08 -0800

    plus

--- project02/src/main/java/com/esickert/domainentities/User.java
+++ project02/src/main/java/com/esickert/domainentities/User.java
@@ -3,5 +3,31 @@ package com.esickert.domainentities;
 /**
  * Created by esickert on 1/1/2017.
  */
-public class User {
-}
+
+public class User {  //a class
+//    private String username;
+    private String username;
+//    private String password;
+    private String password;
+
+
+    public User()  {  // a constructor
+        username = "username";
+        password = "password";
+    }  //end of constructor
+
+
+    public User(String username,String password)    {
+       this.username = username;
+       this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+}  //end of class User
--- project02/src/test/java/com/UserTest.java
+++ project02/src/test/java/com/UserTest.java
@@ -1,15 +1,62 @@
 package com;
 
-import com.esickert.domainentities.User;
+import  com.esickert.domainentities.User;
+//import com.esickert.domainentities.User;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+//import com.esickert.domainentities.User;
 
 /**
  * Created by esickert on 1/1/2017.
  */
 public class UserTest {
+
     @Test
     public void canConstructNewUser()   {
         User user = new User();
+        System.out.println("First instance of 'user'");
     }
 
+    @Test
+    public void userHasDefaultValues() {
+
+        User user = new User();
+
+/*        Assert.assertEquals("Default username expectred",
+                "username",
+                user.getUsername());
+
+        Assert.assertEquals("Default password expected",
+                "password",
+                user.getPassword()); */
+
+        assertEquals("another username expected",
+                "username",
+                user.getUsername());
+
+        assertEquals("and another password",
+                "admin",
+                user.getPassword());
+    }
+
+    @Test
+    public void defineUsernameAndPasswordTest() {
+
+        User user = new User("erich","admin");
+
+        assertEquals("new usernamne",
+                "erich",
+                user.getUsername());
+        System.out.println(user.getUsername());
+
+        assertEquals("new password",
+                "admin",
+                user.getPassword());
+        System.out.println(user.getPassword());
+
+
+    }
 }
+
+
